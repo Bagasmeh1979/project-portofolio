52db335ad27297832a320ef105f479b9b6f6585c
Bagasmeh1979 <bagaspangestu137@gmail.com>
2025-02-13 08:08:27 +0700

    menambahkan fitur toggle

--- script.js
+++ script.js
@@ -6,18 +6,29 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("closePopup").addEventListener("click", function () {
     document.getElementById("popupMessage").style.display = "none";
   });
+
+  const contactForm = document.getElementById("contactForm");
+  const popupSend = document.getElementById("popupSend");
+  const closePopup = document.getElementById("closePopup");
+
+  if (contactForm && popupSend) {
+    contactForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      popupSend.style.display = "block";
+    });
+
+    closePopup.addEventListener("click", () => {
+      popupSend.style.display = "none";
+      location.reload();
+    });
+  }
 });
 
-const contactForm = document.getElementById("contactForm");
-const popupSend = document.getElementById("popupSend");
-const closePopup = document.getElementById("closePopup");
-
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  popupSend.style.display = "block";
-});
-
-closePopup.addEventListener("click", () => {
-  popupSend.style.display = "none";
-  location.reload();
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  var menu = document.getElementById("mobile-menu");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
 });
